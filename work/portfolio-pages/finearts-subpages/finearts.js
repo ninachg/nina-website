0a5ca586fe0a09f34a80c006750c90c0cb0e6987
ninachg <97476287+ninachg@users.noreply.github.com>
2022-02-08 23:26:18 -0500

    11:26

--- work/portfolio-pages/finearts-subpages/finearts.js
+++ work/portfolio-pages/finearts-subpages/finearts.js
@@ -1,12 +1,102 @@
 $(document).ready(function(){
-    $("#backmobiledriveway").click(function(){
-        console.log("clickback")
-        window.location.replace("google.com")
+   
+    $("#backmobile-moth").click(function(){
+        console.log("hi")
+        window.location.replace("coffee.html")
     })
-
-    $("#nextmobiledriveway").click(function(){
-        console.log("click forward with id")
-        window.location.replace("/portfolio/portfolio-pages/finearts-subpages/fennic.html")
+    $("#nextmobile-moth").click(function(){
+        window.location.replace("landscape.html")
     })
     
-    });
+
+
+    $("#backmobile-landscape").click(function(){
+        window.location.replace("moth.html")
+    })
+    $("#nextmobile-landscape").click(function(){
+        window.location.replace("fish.html")
+    })
+    
+
+
+    $("#backmobile-fish").click(function(){
+        window.location.replace("landscape.html")
+    })
+    $("#nextmobile-fish").click(function(){
+        window.location.replace("house.html")
+    })
+    
+
+
+    $("#backmobile-house").click(function(){
+        window.location.replace("fish.html")
+    })
+    $("#nextmobile-house").click(function(){
+        window.location.replace("skeleton.html")
+    })
+
+
+
+    $("#backmobile-skeleton").click(function(){
+        window.location.replace("house.html")
+    })
+    $("#nextmobile-skeleton").click(function(){
+        window.location.replace("driveway.html")
+    })
+
+
+
+    $("#backmobile-driveway").click(function(){
+        window.location.replace("skeleton.html")
+    })
+    $("#nextmobile-driveway").click(function(){
+        window.location.replace("octopus.html")
+    })
+
+
+
+    $("#backmobile-octopus").click(function(){
+        window.location.replace("driveway.html")
+    })
+    $("#nextmobile-octopus").click(function(){
+        window.location.replace("deer.html")
+    })
+
+
+
+    $("#backmobile-deer").click(function(){
+        window.location.replace("octopus.html")
+    })
+    $("#nextmobile-deer").click(function(){
+        window.location.replace("fennic.html")
+    })
+
+
+    $("#backmobile-fennic").click(function(){
+        window.location.replace("deer.html")
+    })
+    $("#nextmobile-fennic").click(function(){
+        window.location.replace("dogs.html")
+    })
+
+
+
+    $("#backmobile-dogs").click(function(){
+        window.location.replace("fennic.html")
+    })
+    $("#nextmobile-dogs").click(function(){
+        window.location.replace("coffee.html")
+    })
+
+
+
+    $("#backmobile-coffee").click(function(){
+        window.location.replace("dogs.html")
+    })
+    $("#nextmobile-coffee").click(function(){
+        window.location.replace("moth.html")
+    })
+    })
+
+
+
