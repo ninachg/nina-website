0dc7b5d4ed297a05792a1cb77996f3b88ec86c28
ninachg <97476287+ninachg@users.noreply.github.com>
2022-02-09 01:20:45 -0500

    1:20

---
+++ work/portfolio-pages/pet-portraits-subpages.html/pet-portraits.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+   
+    $("#backmobile-ruby").click(function(){
+        window.location.replace("sad-cat.html")
+    })
+    $("#nextmobile-ruby").click(function(){
+        window.location.replace("dog.html")
+    })
+    
+
+
+    $("#backmobile-dog").click(function(){
+        window.location.replace("ruby.html")
+    })
+    $("#nextmobile-dog").click(function(){
+        window.location.replace("sarah.html")
+    })
+    
+
+
+    $("#backmobile-sarah").click(function(){
+        window.location.replace("dog.html")
+    })
+    $("#nextmobile-sarah").click(function(){
+        window.location.replace("fluffy-white-dog.html")
+    })
+    
+
+
+    $("#backmobile-fluffy-white-dog").click(function(){
+        window.location.replace("sarah.html")
+    })
+    $("#nextmobile-fluffy-white-dog").click(function(){
+        window.location.replace("german-shepards.html")
+    })
+
+
+
+    $("#backmobile-german-shepards").click(function(){
+        window.location.replace("fluffy-white-dog.html")
+    })
+    $("#nextmobile-german-shepards").click(function(){
+        window.location.replace("dodger.html")
+    })
+
+
+
+    $("#backmobile-dodger").click(function(){
+        window.location.replace("german-shepards.html")
+    })
+    $("#nextmobile-dodger").click(function(){
+        window.location.replace("pitbull.html")
+    })
+
+
+
+    $("#backmobile-pitbull").click(function(){
+        window.location.replace("dodger.html")
+    })
+    $("#nextmobile-pitbull").click(function(){
+        window.location.replace("min-poodle.html")
+    })
+
+
+
+    $("#backmobile-min-poodle").click(function(){
+        window.location.replace("pitbull.html")
+    })
+    $("#nextmobile-min-poodle").click(function(){
+        window.location.replace("sad-cat.html")
+    })
+
+
+    $("#backmobile-sad-cat").click(function(){
+        window.location.replace("min-poodle.html")
+    })
+    $("#nextmobile-sad-cat").click(function(){
+        window.location.replace("ruby.html")
+    })
+
+
+    })
+
+
+
